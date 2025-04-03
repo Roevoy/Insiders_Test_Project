e5d466eb772492ded17b7fc7e7aaa91a7313605d
Roevoy <roevoy@gmail.com>
2025-04-03 18:29:26 +0300

    Segregation.

--- ClassLibraryIntegration/Taxrate.cs
+++ ClassLibraryIntegration/Taxrate.cs
@@ -13,30 +13,4 @@ namespace ClassLibraryIntegration
             return taxRateInfo;
         }
     }
-    public class UsageData
-    {
-        public int UsagePct { get; set; }
-        public string Usage { get; set; }
-        public string Quota { get; set; }
-    }
-
-    public class TaxRateInfo
-    {
-        public string Result { get; set; }
-        public string Found { get; set; }
-        public string Msg { get; set; }
-        public string City { get; set; }
-        public string County { get; set; }
-        public string State { get; set; }
-        public string Country { get; set; }
-        public string TaxName { get; set; }
-        public double Rate { get; set; }
-        public double RatePct { get; set; }
-        public string RateSign { get; set; }
-        public object RateState { get; set; }
-        public object RateCity { get; set; }
-        public object RateCounty { get; set; }
-        public object RateSpecial { get; set; }
-        public UsageData UsageData { get; set; }
-    }
 }
---
+++ ClassLibraryIntegration/UsageData.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ClassLibraryIntegration
+{
+    public class UsageData
+    {
+        public int UsagePct { get; set; }
+        public string Usage { get; set; }
+        public string Quota { get; set; }
+    }
+}
