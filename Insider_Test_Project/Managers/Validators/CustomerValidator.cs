56465ca173f81ff941b97ff214c5a559f879f118
Roevoy <roevoy@gmail.com>
2025-04-04 11:16:04 +0300

    Added customer validator.

---
+++ Insider_Test_Project/Managers/Validators/CustomerValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Insiders_Test_Project.Models;
+
+namespace Insiders_Test_Project.Managers.Validators
+{
+    internal class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(cust => cust.Id).NotEmpty().WithMessage("Customer ID is required.");
+            RuleFor(cust => cust.UserId).NotEmpty().WithMessage("User's profile ID is required.");
+        }
+    }
+}
---
+++ Insider_Test_Project/Managers/Validators/OrderValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insiders_Test_Project.Managers.Validators
+{
+    internal class OrderValidator
+    {
+    }
+}
