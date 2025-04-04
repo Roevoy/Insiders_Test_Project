de157588e065d03011a34f33004069921216b2db
Roevoy <roevoy@gmail.com>
2025-04-04 11:27:42 +0300

    Product validator added.

--- Insider_Test_Project/Managers/Validators/ProductValidator.cs
+++ Insider_Test_Project/Managers/Validators/ProductValidator.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Insiders_Test_Project.Models;
 
 namespace Insiders_Test_Project.Managers.Validators
 {
-    internal class ProductValidator
+    internal class ProductValidator : AbstractValidator<Product>
     {
+        public ProductValidator() 
+        {
+        RuleFor(prod => prod.Id).NotEmpty().WithMessage("Product ID is required.");
+            RuleFor(prod => prod.Name)
+                    .NotEmpty().WithMessage("Product name is required.")
+                    .MaximumLength(50).WithMessage("Product name lenght must be less then 50 symbols.");
+            RuleFor(prod => prod.Description)
+                .NotEmpty().WithMessage("Product description is required.")
+                .MaximumLength(300).WithMessage("Decription must be not more then 300 symbols.");
+            RuleFor(prod => prod.Price)
+                .NotEmpty().WithMessage("Price is required.");
+        }
     }
 }
