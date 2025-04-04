eab22b26b035972810780a458a0043db47ed85de
Roevoy <roevoy@gmail.com>
2025-04-04 11:35:51 +0300

    User validator added.

--- Insider_Test_Project/Managers/Validators/UserValidator.cs
+++ Insider_Test_Project/Managers/Validators/UserValidator.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using Insiders_Test_Project.Models;
 
 namespace Insiders_Test_Project.Managers.Validators
 {
-    internal class UserValidator
+    internal class UserValidator : AbstractValidator<User>
     {
+        public UserValidator() 
+        {
+            RuleFor(user => user.Id).NotEmpty().WithMessage("User ID is required.");
+            RuleFor(user => user.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MinimumLength(2).WithMessage("Name must contain at least 2 symbols.")
+                .MaximumLength(50).WithMessage("Name can't be more then 50 symbols lenght.");
+            RuleFor(user => user.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email is invalid.")
+                .MaximumLength(200).WithMessage("Email can't be more then 200 symbols.");
+            RuleFor(user => user.PasswordHash)
+                .NotEmpty().WithMessage("Password hash is required.");
+        }
     }
 }
