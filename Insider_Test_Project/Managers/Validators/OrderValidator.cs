f317c5253b22973aa4f6d8e0ed5fd2ce87e082a5
Roevoy <roevoy@gmail.com>
2025-04-04 11:20:44 +0300

    Order validator added.

--- Insider_Test_Project/Managers/Validators/OrderValidator.cs
+++ Insider_Test_Project/Managers/Validators/OrderValidator.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using FluentValidation;
+using Insiders_Test_Project.Models;
 
 namespace Insiders_Test_Project.Managers.Validators
 {
-    internal class OrderValidator
+    internal class OrderValidator : AbstractValidator<Order>
     {
+        public OrderValidator() {
+
+            RuleFor(order => order.Id).NotEmpty().WithMessage("Order ID is required.");
+            RuleFor(order => order.CreatedDate).NotEmpty().WithMessage("Date of creating is required.");
+            RuleFor(order => order.CustomerId).NotEmpty().WithMessage("Customer ID is required.");
+        }
     }
 }
